81f83057a450676b0620deaba5ef8f8e51fa0169
inkozz <brion.benjamin@outlook.com>
2022-07-18 10:23:50 +0200

    add

--- src/data/getData.js
+++ src/data/getData.js
@@ -1,38 +1,35 @@
 import axios from 'axios';
 
 export const getUsers = async () => {
-    const { data } = await axios.get('http://localhost:8080/users');
-    return data;
+  const { data } = await axios.get('http://localhost:8080/users');
+  return data;
 };
 
 export const getUser = async (id) => {
-    const { data } = await axios.get(`http://localhost:8080/users/${id}`);
-    return data;
+  const { data } = await axios.get(`http://localhost:8080/users/${id}`);
+  return data;
 };
 
-export const add = async (user) => {
-    const { data } = await axios.post('http://localhost:8080/users', user);
-    return data;
+export const addUser = async (user) => {
+  const { data } = await axios.post('http://localhost:8080/users', user);
+  return data;
 };
 
 export const update = async (user) => {
-    const { data } = await axios.put(
-        `http://localhost:8080/users/${user.id}`,
-        user,
-    );
-    return data;
+  const { data } = await axios.put(`http://localhost:8080/users/${user.id}`, user);
+  return data;
 };
 
 export const remove = async (user) => {
-    const { data } = await axios.delete(`http://localhost:8080/users/${user.id}`);
-    return data;
+  const { data } = await axios.delete(`http://localhost:8080/users/${user.id}`);
+  return data;
 };
 
 export const getTasks = async () => {
-    const { data } = await axios.get('http://localhost:8080/tasks');
-    return data;
+  const { data } = await axios.get('http://localhost:8080/tasks');
+  return data;
 };
 export const getProjects = async () => {
-    const { data } = await axios.get('http://localhost:8080/projects');
-    return data;
+  const { data } = await axios.get('http://localhost:8080/projects');
+  return data;
 };

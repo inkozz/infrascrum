99c38381d49d9de5f76163d38828197836d71baf
inkozz <brion.benjamin@outlook.com>
2022-07-23 17:42:24 +0200

    update

--- src/components/ui/SideBar.jsx
+++ src/components/ui/SideBar.jsx
@@ -73,6 +73,49 @@ const Sidebar = () => {
             </NavLink>
           </li>
         </ul>
+        {isLogged ? (
+          <button
+            type="button"
+            className="flex items-center text-blue-600 duration-200 hover:text-blue-800"
+            onClick={logout}
+            data-cy="logoutBtn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-6 w-6 ${!isExpand ? 'mr-0' : 'mr-2'}`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+              />
+            </svg>
+            {isExpand && 'Se déconnecter'}
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="flex items-center text-blue-600 duration-200 hover:text-blue-800"
+            onClick={login}
+            data-cy="loginBtn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-6 w-6 ${!isExpand ? 'mr-0' : 'mr-2'}`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+              />
+            </svg>
+            {isExpand && 'Se connecter'}
+          </button>
+        )}
       </nav>
     </div>
   );

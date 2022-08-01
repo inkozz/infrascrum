a4f2b348424d06ba798fec022110795b3a01c360
inkozz <brion.benjamin@outlook.com>
2022-08-01 14:33:32 +0200

    update

--- src/components/forms/FormLogin.jsx
+++ src/components/forms/FormLogin.jsx
@@ -1,21 +1,42 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/no-unescaped-entities */
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import toast from 'react-hot-toast';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import loginCtx from '../../loginCtx';
 import { postLogin } from '../../data/getData';
 
-const LoginForm = ({ formik }) => {
+const LoginForm = () => {
+  const navigate = useNavigate();
   const { isLogged, setIsLogged } = useContext(loginCtx);
-  const login = () => {
-    postLogin(formik.values);
-    console.log(formik.values);
-    localStorage.setItem('isLogged', true);
-
-    setIsLogged(true);
-    toast('Connecté !', { className: 'successToast' });
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: 'test',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Email invalide').required('Entrez un Email'),
+      password: Yup.string().required('Entrez un mot de passe'),
+    }),
+    onSubmit: async (values) => {
+      const tokenRes = await postLogin(values);
+      console.log('token res :::', tokenRes);
+      if (tokenRes) {
+        localStorage.setItem('isLogged', true);
+        console.log('verif tokenres.data :::', tokenRes);
+        setIsLogged(true);
+        toast('Connecté', { className: 'successToast' });
+        navigate('/home');
+      } else {
+        console.log('coucou le else :::', tokenRes);
+        localStorage.clear('isLogged');
+        navigate('/');
+      }
+    },
+  });
+  useEffect(() => {}, [isLogged, setIsLogged]);
   return (
     <div
       className="h-full w-full flex justify-center items-center"
@@ -28,7 +49,7 @@ const LoginForm = ({ formik }) => {
           <img src="../public/img/LogoLogin.png" alt="logo" />
           <h2 className="text-2xl font-light italic text-primary ">Login</h2>
         </div>
-        <form onSubmit={formik.onSubmit} method="POST">
+        <form onSubmit={formik.handleSubmit}>
           <div className="flex flex-col p-4">
             <label htmlFor="email" className="text-sm text-gray-700 flex flex-col ">
               Nom d'utilisateur
@@ -66,16 +87,16 @@ const LoginForm = ({ formik }) => {
             </div>
           </div>
 
-          <Link to="/home">
-            <div className="flex flex-col items-center justify-center ">
-              <button
-                onClick={login}
-                className=" w-64 rounded bg-cyan-500 hover:bg-primary text-white text-lg p-2"
-                type="submit">
-                Se connecter
-              </button>
-            </div>
-          </Link>
+          <div className="flex flex-col items-center justify-center ">
+            <button
+              disabled={!(formik.isValid && formik.dirty)}
+              // onClick={login}
+              className=" btn disabled w-64 rounded bg-cyan-500 hover:bg-primary text-white text-lg p-2"
+              type="submit">
+              Se connecter
+            </button>
+          </div>
+
           <div className="pt-8">
             <Link to="/adduser">
               <p className="text-primary text-center text-sm underline decoration-double">

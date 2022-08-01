3933e576defc3f5d45e5212dfcf86a3b59306bf9
inkozz <brion.benjamin@outlook.com>
2022-08-01 18:21:01 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        retry: 1,
+        retry: 0,
         refetchOnWindowFocus: false,
       },
     },

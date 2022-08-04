a82bec375a1c58cfc3763a45699e2f9a8ba22e58
inkozz <brion.benjamin@outlook.com>
2022-08-04 16:12:21 +0200

    update homepage color

--- src/components/home/TaskItemHome.jsx
+++ src/components/home/TaskItemHome.jsx
@@ -12,8 +12,11 @@ const TaskItemHome = ({ data }) => {
 
   return (
     <div>
-      <div className="bg-sky-200 w-64 h-40 m-4 rounded-lg">
-        <div className="bg-white w-64 h-40 mt-2 hover:m-0 absolute rounded-lg shadow-lg hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
+      <div
+        className={`${
+          data.priority === 'High' ? 'bg-green' : 'bg-red'
+        } w-64 h-40 m-4 rounded-lg`}>
+        <div className="bg-white w-64 h-40 mt-2 hover:m-0 absolute rounded-lg shadow-lg hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in">
           <h1 className="text-end text-xl p-2 font-bold">{data.priority}</h1>
           <div className=" font-semibold text-primary m-4">
             <p className="text-2xl">{data.name}</p>
